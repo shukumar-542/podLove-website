6eb8b9a9febc6d6ea008c2c47275dfd5e452cb31
shukumar-542 <shukumar542@gmail.com>
2025-02-21 12:45:36 +0600

    Implement update profile image

--- src/page/EditProfile/EditProfile.jsx
+++ src/page/EditProfile/EditProfile.jsx
@@ -1,15 +1,49 @@
-import React from "react";
-import bg from "../../assets/b.png";
+import React, { useState } from "react";
 import profile from "../../assets/profile.png";
-import { Form, Input } from "antd";
+import { Form, Input, Upload, Avatar, message } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import AuthButton from "../../component/AuthButton/AuthButton";
+import { CameraOutlined } from "@ant-design/icons"
 const EditProfile = () => {
-  return (
-    <div className="px-2 md:px-0 bg-[#FAF2EF] flex items-center justify-center">
-      <div className="bg-white  shadow-md shadow-[#eb8b73] my-10 p-5 rounded-md w-full px-2 md:px-10 md:w-1/3">
+    const [imageUrl, setImageUrl] = useState(null);
 
-        <img src={profile} className="h-32 w-32 rounded-full relative -top-16 left-28 md:left-52" alt="" />
+    const handleImageChange = (info) => {
+        if (info.file.status === "done" || info.file.status === "uploading") {
+          const reader = new FileReader();
+          reader.onload = () => setImageUrl(reader.result);
+          reader.readAsDataURL(info.file.originFileObj);
+        }
+      };
+
+
+      const beforeUpload = (file) => {
+        const isImage = file.type.startsWith("image/");
+        if (!isImage) {
+          message.error("You can only upload image files!");
+        }
+        return isImage;
+      };
+
+  return (
+    <div className="px-2 md:px-0 bg-[#FAF2EF] flex items-center justify-center py-10">
+      <div className="bg-white  shadow-md shadow-[#eb8b73] my-10 p-5 rounded-md w-full px-2 md:px-10 md:w-1/3 ">
+
+        {/* <img src={profile} className="h-32 w-32 rounded-full relative -top-16 left-28 md:left-52" alt="" /> */}
+
+        <div className="h-32 w-32 rounded-full relative -top-16 left-28 md:left-52">
+          <Upload
+            showUploadList={false}
+            beforeUpload={beforeUpload}
+            onChange={handleImageChange}
+          >
+            <Avatar
+              size={128}
+              src={imageUrl ? imageUrl : profile}
+              className="cursor-pointer"
+              icon={<CameraOutlined />}
+            />
+          </Upload>
+        </div>
         <Form layout="vertical">
           <div className="md:flex  gap-5 w-full ">
             <Form.Item label={"First Name"}  className="w-full">
