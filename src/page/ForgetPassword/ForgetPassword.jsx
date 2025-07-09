0f6ba70ad83cc4daa46227282358745260d49975
shukumar-542 <shukumar542@gmail.com>
2025-07-09 17:37:44 +0600

    Implement forget password api

--- src/page/ForgetPassword/ForgetPassword.jsx
+++ src/page/ForgetPassword/ForgetPassword.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import img from "../../assets/ForgotPassword.png"
+import img from "../../assets/ForgotPassword.png";
 import { Form, Input } from "antd";
 import AuthButton from "../../component/AuthButton/AuthButton";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { toast } from "sonner";
+import { useForgetPasswordMutation } from "../../redux/Api/AuthApi";
 const ForgetPassword = () => {
-  
+  const navigate = useNavigate();
+  const [forgetPassword, { isLoading }] = useForgetPasswordMutation();
+  const onFinish = (values) => {
+    const data = {
+      email: values?.email,
+    };
+    forgetPassword(data)
+      .unwrap()
+      .then((payload) => {
+        // console.log("fulfilled", payload)
+        toast.success("Please check you email!");
+        localStorage.setItem("email", values.email);
+        navigate("/verify-forget-otp");
+      })
+      .catch((error) => toast.error(error?.data?.message));
+  };
   return (
     <div
       style={{
@@ -21,22 +37,23 @@ const ForgetPassword = () => {
       {/* 🔹 Content (above the overlay) */}
       <div className="flex items-center justify-start max-w-5xl mx-auto h-full p-2 md:p-0 relative z-10">
         <div className="bg-white shadow-2xl shadow-[#F26828] rounded-md p-5 md:p-10 max-w-5xl">
-          <p className="text-2xl font-bold text-[#333333] text-center ">Forgot Password</p>
-          <p className="mt-2">Enter your email and we will send a link to set a 
-          new password</p>
+          <p className="text-2xl font-bold text-[#333333] text-center ">
+            Forgot Password
+          </p>
+          <p className="mt-2">
+            Enter your email and we will send a link to set a new password
+          </p>
 
-          <Form layout="vertical mt-10">
+          <Form layout="vertical mt-10" onFinish={onFinish}>
             <Form.Item label="Email">
-              <Input placeholder="Enter your email here" className="border-[#FFA175]" />
+              <Input
+                placeholder="Enter your email here"
+                className="border-[#FFA175]"
+              />
             </Form.Item>
-            <Link to={'/verify-forget-otp'}>
               <AuthButton className={"py-2"}>Send</AuthButton>
-            </Link>
           </Form>
-
-
-
-        </div> 
+        </div>
       </div>
     </div>
   );
