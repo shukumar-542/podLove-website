c9e15affcbf01b66180b70c7c838eaddbe65d1df
shukumar-542 <shukumar542@gmail.com>
2025-07-26 14:40:12 +0600

    Implement attention page

--- src/page/AttentionError/AttentionError.jsx
+++ src/page/AttentionError/AttentionError.jsx
@@ -1,21 +1,28 @@
 import { Link } from "react-router";
 import logo from "../../assets/podLogo.png";
+import attention from "../../assets/attention.png";
 import { IoArrowBack } from "react-icons/io5";
 
 const AttentionError = () => {
     return (
         <div className="p-6 bg-[#FCE7D3] min-h-screen ">
-            <div className="max-w-6xl mx-auto">
+            <div className="max-w-6xl mx-auto h-[100%]">
 
                 <img src={logo} className="h-10" alt="" />
                 <div className="flex items-center gap-2 text-xl mt-5">
                     <Link to={-1}>
-                    <IoArrowBack />
+                        <IoArrowBack />
                     </Link>
                     <p>Attention!</p>
                 </div>
-                <div className="flex items-center justify-center">
-
+                <div className="flex items-center justify-center flex-col h-[60vh]">
+                        <img src={attention} className="h-36" alt="" />
+                        <h1 className="text-3xl font-medium my-5">Attention!</h1>
+                        <p className="text-center max-w-3xl">Thank you for your interest in PodLove. Based on your responses, it seems our
+                            app may not be the right fit for you at this time. We appreciate
+                            you exploring our platform and wish you the best in
+                            finding the connections you're looking for. Feel free to
+                            check back with us in the future!</p>
                 </div>
             </div>
         </div>
