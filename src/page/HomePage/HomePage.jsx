2915d28e687382045b847e1fb4f254f9804608e5
TayeburRahman <tayebrayhan101+@gmail.com>
2025-10-25 15:28:29 +0600

    updates all

--- src/page/HomePage/HomePage.jsx
+++ src/page/HomePage/HomePage.jsx
@@ -136,11 +136,11 @@ const HomePage = () => {
       return "Request a Podcast";
     }
     switch (status) {
-      case "Scheduled": // only show date and time
+      case "Scheduled":
         return "Scheduled";
-      case "Playing": // when playing when show button to (Join Now)
+      case "Playing":
         return "Join Now";
-      case "StreamStart": // when playing when show button to (Join Now)
+      case "StreamStart":
         return "Join Now";
       case "Done":
         return "Join Now";
@@ -163,32 +163,17 @@ const HomePage = () => {
     new Date() >= new Date(getPodcastDetails.data.user.chatingtime);
 
 
-    const userId =  getUser?.data?._id;
+  const userId = getUser?.data?._id;
 
-    console.log('status', status);
-    console.log('podcast', podcast);
-    console.log('getPodcastDetails?.data', getPodcastDetails?.data);
+  // console.log('status', status);
+  // console.log('podcast', podcast);
+  // console.log('getPodcastDetails?.data', getPodcastDetails?.data);
 
-    const isRequested = podcast?.participants?.some(participant => participant?.isRequest && participant?._id === userId);
+  const isRequested = podcast?.participants?.some(participant => participant?.isRequest && participant?._id === userId);
 
-    console.log('isRequested', isRequested);
- 
   return (
     <div className="bg-[#F7E8E1]">
       <div className="container mx-auto py-14">
-        {/* Create New Match */}
-        {/* <div className="flex justify-center">
-          {podcast?.participants?.length === 0 && (
-            <button
-              onClick={handleCreatePodcast}
-              className="bg-[#F68064] w-full max-w-[200px] mt-2 py-2 mb-8 rounded-md text-white text-xl"
-            >
-              Create New Match
-            </button>
-          )}
-        </div> */}
-
-        {/* Matches Section */}
         {isLoading ? (
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10">
             {[...Array(4)].map((_, i) => (
@@ -202,10 +187,8 @@ const HomePage = () => {
           <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-10 px-4 md:px-0">
             {podcast?.participants?.map((participant, i) => (
               <div key={participant?._id}>
-
                 {
                   getPodcastDetails?.data?.user?.subscription?.fee === 'Free' ?
-
                     <div className="cursor-pointer hover:shadow-2xl rounded-br-3xl relative">
                       <img src={img1} className="w-full" alt="" />
                       <p className="absolute bottom-6 md:bottom-10 right-[32%] md:right-[45%] text-xl font-semibold">
@@ -259,182 +242,174 @@ const HomePage = () => {
           </div>
         )}
 
+        {podcast?.primaryUser?._id && (
+          <>
+            <h1 className="text-center font-bold text-2xl md:text-4xl my-14">
+              My Podcast Schedule
+            </h1>
+            {/* Video + Join / Request Button Section */}
+            <section className="md:mb-20 relative">
+              <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="md:h-[500px] mx-auto rounded-md"
+              >
+                <source src={video} type="video/mp4" />
+              </video>
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+                <img src={mic} alt="Microphone" className=" w-10 md:w-24 h-10 md:h-24 mx-auto" />
+                <h1 className="md:text-4xl font-poppins text-white">Date & Time:</h1>
 
-        {
-          podcast?.primaryUser?._id  && (
-         <> 
-          <h1 className="text-center font-bold text-2xl md:text-4xl my-14">
-           My Podcast Schedule
-         </h1>
-        {/* Video + Join / Request Button Section */}
-        <section className="md:mb-20 relative">
-          <video
-            autoPlay
-            loop
-            muted
-            playsInline
-            className="md:h-[500px] mx-auto rounded-md"
-          >
-            <source src={video} type="video/mp4" />
-          </video>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-            <img src={mic} alt="Microphone" className=" w-10 md:w-24 h-10 md:h-24 mx-auto" />
-            <h1 className="md:text-4xl font-poppins text-white">Date & Time:</h1>
-
-            {/* Conditional Button */}
-            {status === "NotScheduled" || (podcast?.questionsStatus === "1stDone" && status === "Finished") ? (
-              <button
-                onClick={handleRequestPodcast}
-                disabled={requestPodcastLoading}
-                className="w-full mt-2 py-2 rounded-md bg-[#FFA175] text-white hover:bg-[#e68b5a] transition duration-300"
-              > 
-                {getButtonLabel()}  {requestPodcastLoading && <Spin></Spin>}
-              </button>
-            ): status === "ReqScheduled" && isRequested !== true? ( 
-               <button
-                  disabled={isRequested === true}
-                  onClick={handleRequestPodcast}
-                  className={`w-full mt-2 py-2 rounded-md transition duration-300 
+                {status === "NotScheduled" || (podcast?.questionsStatus === "1stDone" && status === "Finished") ? (
+                  <button
+                    onClick={handleRequestPodcast}
+                    disabled={requestPodcastLoading}
+                    className="w-full mt-2 py-2 rounded-md bg-[#FFA175] text-white hover:bg-[#e68b5a] transition duration-300"
+                  >
+                    {getButtonLabel()}  {requestPodcastLoading && <Spin></Spin>}
+                  </button>
+                ) : status === "ReqScheduled" && isRequested !== true ? (
+                  <button
+                    disabled={isRequested === true}
+                    onClick={handleRequestPodcast}
+                    className={`w-full mt-2 py-2 rounded-md transition duration-300 
                   ${!isRequested
-                      ? "bg-[#F68064] text-white hover:bg-[#e76a4f]"
-                      : "bg-gray-200 text-gray-500 cursor-not-allowed"
-                    }`}
-                >
-                  Request a Podcast
-                </button>
-              
-            ) : (
-              <div>
-                <h2 className=" text-white">{getPodcastDetails?.data?.podcast?.schedule?.date} {getPodcastDetails?.data?.podcast?.schedule?.day} {getPodcastDetails?.data?.podcast?.schedule?.time}</h2>
-                <button
-                  disabled={!isJoinEnabled}
-                  onClick={isJoinEnabled ? handleVideoCall : undefined}
-                  className={`w-full mt-2 py-2 rounded-md transition duration-300 
-                  ${isJoinEnabled
-                      ? "bg-[#F68064] text-white hover:bg-[#e76a4f]"
-                      : "bg-gray-200 text-gray-500 cursor-not-allowed"
-                    }`}
-                >
-                  {getButtonLabel()}
-                </button>
+                        ? "bg-[#F68064] text-white hover:bg-[#e76a4f]"
+                        : "bg-gray-200 text-gray-500 cursor-not-allowed"
+                      }`}
+                  >
+                    Request a Podcast
+                  </button>
+
+                ) : (
+                  <div>
+                    <h2 className=" text-white">{getPodcastDetails?.data?.podcast?.schedule?.date} {getPodcastDetails?.data?.podcast?.schedule?.day} {getPodcastDetails?.data?.podcast?.schedule?.time}</h2>
+                    <button
+                      disabled={!isJoinEnabled}
+                      onClick={isJoinEnabled ? handleVideoCall : undefined}
+                      className={`w-full mt-2 py-2 rounded-md transition duration-300 ${isJoinEnabled
+                          ? "bg-[#F68064] text-white hover:bg-[#e76a4f]"
+                          : "bg-gray-200 text-gray-500 cursor-not-allowed"
+                        }`}
+                    >
+                      {getButtonLabel()}
+                    </button>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-        </section> 
-            </> 
-          )
+            </section>
+          </>
+        )
         }
 
         {
-          podcast?.participants?.length === 0 && getPodcastDetails?.data?.podcast?.participants.length === 0 (
-          <section className="md:mb-20 relative">
-          <video
-            autoPlay
-            loop
-            muted
-            playsInline
-            className="md:h-[500px] mx-auto rounded-md"
-          >
-            <source src={video} type="video/mp4" />
-          </video>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-            <img src={mic} alt="Microphone" className=" w-10 md:w-24 h-10 md:h-24 mx-auto" />  
-              <div>
-                <h2 className=" text-white">{getPodcastDetails?.data?.podcast?.schedule?.date} {getPodcastDetails?.data?.podcast?.schedule?.day} {getPodcastDetails?.data?.podcast?.schedule?.time}</h2>
+          podcast?.participants?.length === 0 && getPodcastDetails?.data?.podcast?.participants.length === 0(
+            <section className="md:mb-20 relative">
+              <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="md:h-[500px] mx-auto rounded-md"
+              >
+                <source src={video} type="video/mp4" />
+              </video>
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+                <img src={mic} alt="Microphone" className=" w-10 md:w-24 h-10 md:h-24 mx-auto" />
+                <div>
+                  <h2 className=" text-white">{getPodcastDetails?.data?.podcast?.schedule?.date} {getPodcastDetails?.data?.podcast?.schedule?.day} {getPodcastDetails?.data?.podcast?.schedule?.time}</h2>
                   <h5 className="text-gray-200 text-sm md:text-lg ">
-                      It doesn’t seem like a perfect match yet for your friend.
+                    It doesn’t seem like a perfect match yet for your friend.
                   </h5>
-              </div> 
-          </div>
-        </section>
+                </div>
+              </div>
+            </section>
           )
         }
-      
+        <>
+          {getPodcastDetails?.data?.podcast?.participants?.length > 0 && !podcast?.primaryUser?._id && (
+            <h1 className="text-center font-bold text-2xl md:text-4xl my-14">
+              You're Matched With Someone
+            </h1>
+          )}
+          {getPodcastDetails?.data?.podcast?.participants?.length > 0 && !podcast?.primaryUser?._id && (
+            <Carousel arrows infinite={false}>
+              {getPodcastDetails?.data?.podcast?.participants
+                ?.filter((participant) => participant?._id === userId)
+                .map((match) => (
+                  <section key={match?._id} className="relative">
+                    <video
+                      autoPlay
+                      loop
+                      muted
+                      playsInline
+                      className="md:h-[500px] mx-auto rounded-md"
+                    >
+                      <source src={video} type="video/mp4" />
+                    </video>
 
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+                      <img
+                        src={mic}
+                        alt="Microphone"
+                        className="w-10 md:w-24 h-10 md:h-24 mx-auto"
+                      />
+                      <h1 className="md:text-4xl font-poppins text-white">Date & Time:</h1>
 
-    <>
-  {getPodcastDetails?.data?.podcast?.participants?.length > 0  && !podcast?.primaryUser?._id && (
-    <h1 className="text-center font-bold text-2xl md:text-4xl my-14">
-      You're Matched With Someone
-    </h1>
-  )}
-  {getPodcastDetails?.data?.podcast?.participants?.length > 0  && !podcast?.primaryUser?._id && (
-  <Carousel arrows infinite={false}>
-    {getPodcastDetails?.data?.podcast?.participants
-  ?.filter((participant) => participant?._id === userId)  
-  .map((match) => (
-    <section key={match?._id} className="relative">
-      <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="md:h-[500px] mx-auto rounded-md"
-      >
-        <source src={video} type="video/mp4" />
-      </video>
+                      {podcast?.status === "Playing" || podcast?.status === "Done" ? (
+                        <div>
+                          <h2 className="text-white text-center">
+                            {podcast?.schedule?.date} {podcast?.schedule?.day} {podcast?.schedule?.time}
+                          </h2>
+                          <button
+                            onClick={() =>
+                              podcast?.status === "Playing" || podcast?.status === "Done"
+                                ? handleVideoCallForUser(podcast?.roomCodes)
+                                : undefined
+                            }
+                            className={`w-full mt-2 py-2 rounded-md transition duration-300 ${podcast?.status === "Playing" || podcast?.status === "Done"
+                              ? "bg-[#F68064] text-white hover:bg-[#e76a4f]"
+                              : "bg-gray-200 text-gray-500 cursor-not-allowed"
+                              }`}
+                          >
+                            {podcast?.status === "Playing" || podcast?.status === "Done"
+                              ? "Join Now"
+                              : "Not Available"}
+                          </button>
+                        </div>
+                      ) : podcast?.status === "NotScheduled" || podcast?.status === "ReqScheduled" ? (
+                        <button
+                          onClick={handleRequestPodcast}
+                          disabled={match?.isRequest}
+                          className="w-full mt-2 py-2 rounded-md bg-[#FFA175] text-white hover:bg-[#e68b5a] transition duration-300"
+                        >
 
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-        <img
-          src={mic}
-          alt="Microphone"
-          className="w-10 md:w-24 h-10 md:h-24 mx-auto"
-        />
-        <h1 className="md:text-4xl font-poppins text-white">Date & Time:</h1>
+                          {match?.isRequest ? (
+                            "Please Waiting for Schedule"
+                          ) : (
+                            <>
+                              {getButtonLabel()} {requestPodcastLoading && <Spin />}
+                            </>
+                          )}
 
-        {podcast?.status === "Playing" || podcast?.status === "Done" ? (
-          <div>
-            <h2 className="text-white text-center">
-              {podcast?.schedule?.date} {podcast?.schedule?.day} {podcast?.schedule?.time}
-            </h2>
-            <button
-              onClick={() =>
-                podcast?.status === "Playing" || podcast?.status === "Done"
-                  ? handleVideoCallForUser(podcast?.roomCodes)
-                  : undefined
-              }
-              className={`w-full mt-2 py-2 rounded-md transition duration-300 ${
-                podcast?.status === "Playing" || podcast?.status === "Done"
-                  ? "bg-[#F68064] text-white hover:bg-[#e76a4f]"
-                  : "bg-gray-200 text-gray-500 cursor-not-allowed"
-              }`}
-            >
-              {podcast?.status === "Playing" || podcast?.status === "Done"
-                ? "Join Now"
-                : "Not Available"}
-            </button>
-          </div>
-        ) : podcast?.status === "NotScheduled" || podcast?.status === "ReqScheduled" ? (
-          <button
-            onClick={handleRequestPodcast}
-            disabled={match?.isRequest}
-            className="w-full mt-2 py-2 rounded-md bg-[#FFA175] text-white hover:bg-[#e68b5a] transition duration-300"
-          >
+                        </button>
+                      ) : (
+                        <div>
+                          <h2 className="text-white text-center"></h2>
+                          <button className="w-full mt-2 py-2 rounded-md transition duration-300 bg-gray-200 text-gray-500 cursor-not-allowed">
+                            Not Available
+                          </button>
+                        </div>
+                      )}
+                    </div>
+                  </section>
+                ))}
 
-             {match?.isRequest ? (
-     "Please Waiting for Schedule"
-  ) : (
-    <>
-      {getButtonLabel()} {requestPodcastLoading && <Spin />}
-    </>
-  )}
-            
-          </button>
-        ) : (
-          <div>
-            <h2 className="text-white text-center"></h2>
-            <button className="w-full mt-2 py-2 rounded-md transition duration-300 bg-gray-200 text-gray-500 cursor-not-allowed">
-              Not Available
-            </button>
-          </div>
-        )}
-      </div>
-    </section>
-))}
-
-  </Carousel>
-)}
-</>
+            </Carousel>
+          )}
+        </>
 
         {/* Subscription Plans */}
         <h1 className="text-center font-bold text-2xl md:text-4xl mt-16">
