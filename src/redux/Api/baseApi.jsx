8c5f231c220e1c125ed712c4d3720464ff1aa4b2
TayeburRahman <tayebrayhan101+@gmail.com>
2025-10-25 15:49:34 +0600

    updates all

--- src/redux/Api/baseApi.jsx
+++ src/redux/Api/baseApi.jsx
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseQuery = fetchBaseQuery({
 
     // baseUrl: 'http://13.51.233.34:8000', // Live
-    // baseUrl: 'https://backend.podlove.co',
-    baseUrl: 'http://10.10.20.11:8000',
+    baseUrl: 'https://backend.podlove.co',
+    // baseUrl: 'http://10.10.20.11:8000',
     prepareHeaders: (headers) => {
         const token = localStorage.getItem('podlove-token');
         if (token) {
