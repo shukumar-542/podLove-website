9187063c21d7f55f0d5dc05eb5f7a4fcac774086
mdnuruzzamannirob <dev.mdnuruzzaman@gmail.com>
2025-11-27 09:44:27 +0600

    fixed notification

--- src/page/Notification/Notification.jsx
+++ src/page/Notification/Notification.jsx
@@ -3,14 +3,16 @@ import { useGetNotificationQuery } from "../../redux/Api/SubscriptionPlan";
 
 const Notification = () => {
   const { data: getNotification } = useGetNotificationQuery();
+
   return (
-    <div className="bg-[#FAF2EF] min-h-screen">
-      <div className="container mx-auto">
+    <div className="bg-[#FAF2EF] min-h-dvh flex flex-col">
+      <div className="container h-full mx-auto flex-1">
         <p className="text-2xl font-semibold font-poppins text-center py-10">
           Notifications
         </p>
         <p className="text-xl font-semibold">
-          Total {getNotification?.data?.notifications?.length} Notifications
+          Total {getNotification?.data?.notifications?.length ?? 0}{" "}
+          Notifications
         </p>
 
         <div className="space-y-4 mt-10 pb-10">
@@ -20,18 +22,11 @@ const Notification = () => {
                 key={i}
                 className="flex justify-between items-center bg-white p-3 text-[#333333]"
               >
-                <span>{notification?.message[0]?.description}</span>
-                <span>{notification?.message[0]?.title}</span>
+                <span>{notification?.message?.[0]?.description}</span>
+                <span>{notification?.message?.[0]?.title}</span>
               </p>
             );
           })}
-          {/* <p className="flex justify-between items-center bg-white p-3 text-[#333333]">
-                <span>
-                  A new user has completed the vetting process and registered on
-                  PodLove.
-                </span>
-                <span>Just Now</span>
-              </p> */}
         </div>
       </div>
       <IsPodSafe />
