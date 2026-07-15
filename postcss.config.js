f717a836324e386e54da3f3aed4e4b52f6635121
MD.NURUZZAMAN <dev.mdnuruzzaman@gmail.com>
2026-07-15 16:33:02 +0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-};                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-
+};
